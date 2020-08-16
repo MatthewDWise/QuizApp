794c83fe7b87db5b35ce6897aaeb597d3c7ceeb7
MatthewDWise <68114189+MatthewDWise@users.noreply.github.com>
2020-08-16 12:13:37 -0400

    Update STORE.js

--- STORE.js
+++ STORE.js
@@ -3,252 +3,252 @@ const STORE = {
     
   {
     question: "What is the Capital of Alabama?",
-    options: ["Birmingham", "Tuscaloosa","Montgomery", "Dothan", "Hoover"],
+    choices: ["Birmingham", "Tuscaloosa","Montgomery", "Dothan", "Hoover"],
     answer: "Montgomery"},
 
   {
     question: "What is the Capital of Alaska?",
-    options: ["Anchorage","Juneau", "McCarthy", "Bethel", "Fairbanks"],
+    choices: ["Anchorage","Juneau", "McCarthy", "Bethel", "Fairbanks"],
     answer: "Juneau"},
 
   {
      question: "What is the Capital of Arizona?",
-     options: ["Phoenix", "Tucson", "Wickenburg", "Congress", "Bagdad"],
+     choices: ["Phoenix", "Tucson", "Wickenburg", "Congress", "Bagdad"],
      answer: "Phoenix"},
 
   {
     question: "What is the Capital of Arkansas?",
-    options: ["Pine Bluff", "Hot Springs", "Fayetteville", "Jonesboro","Little Rock"],
+    choices: ["Pine Bluff", "Hot Springs", "Fayetteville", "Jonesboro","Little Rock"],
     answer: "Little Rock"},
   
   {
     question: "What is the Capital of California?",
-    options: ["San Diego", "Los Angeles","Sacramento", "San Jose", "Bakersfield"],
+    choices: ["San Diego", "Los Angeles","Sacramento", "San Jose", "Bakersfield"],
     answer: "Sacramento"},
   
   {
     question: "What is the Capital of Colorado?",
-    options: ["Denver", "Colorado Springs", "Grand Junction", "Alamosa", "La Junta"],
+    choices: ["Denver", "Colorado Springs", "Grand Junction", "Alamosa", "La Junta"],
     answer: "Denver"},
 
   {
     question: "What is the Capital of Connecticut?",
-    options: ["Danbury","Hartford", "New Milford", "Middletown", "Bristol"],
+    choices: ["Danbury","Hartford", "New Milford", "Middletown", "Bristol"],
     answer: "Hartford"},
 
   {
     question: "What is the Capital of Delaware?",
-    options: ["New Castle", "Smyrna", "Milford", "Wilmington", "Dover"],
+    choices: ["New Castle", "Smyrna", "Milford", "Wilmington", "Dover"],
     answer: "Dover"},
 
   {
     question: "What is the Capital of Florida?",
-    options: ["Orlando", "Jacksonville", "Tampa", "Tallahassee", "Miami"],
+    choices: ["Orlando", "Jacksonville", "Tampa", "Tallahassee", "Miami"],
     answer: "Tallahassee"},
 
   {
     question: "What is the Capital of Georgia?",
-    options: ["Atlanta", "Alpharetta", "Athens", "Macon", "Savannah"],
+    choices: ["Atlanta", "Alpharetta", "Athens", "Macon", "Savannah"],
     answer: "Atlanta"},
 
   {
     question: "What is the Capital of Hawaii?",
-    options: ["O'ahu","Waipahu", "Honolulu", "Haliewa", "Napili-Honokowai" ],
+    choices: ["O'ahu","Waipahu", "Honolulu", "Haliewa", "Napili-Honokowai" ],
     answer: "Honolulu"},
 
   {
     question: "What is the Capital of Idaho?",
-    options: ["Nampa","Idaho Falls", "Boise", "Twin Falls", "Blackfoot"],
+    choices: ["Nampa","Idaho Falls", "Boise", "Twin Falls", "Blackfoot"],
     answer: "Boise"},
 
   {
     question: "What is the Capital of Illinois?",
-    options: ["Springfield", "Peoria", "Bloomington", "Champaign", "Decatur"],
+    choices: ["Springfield", "Peoria", "Bloomington", "Champaign", "Decatur"],
     answer: "Springfield"},
 
   {
     question: "What is the Capital of Indiana?",
-    options: ["Fort Wayne", "Indianapolis", "Bloomington", "Kokomo", "Lafayette"],
+    choices: ["Fort Wayne", "Indianapolis", "Bloomington", "Kokomo", "Lafayette"],
     answer: "Indianapolis"},
 
   {
     question: "What is the Capital of Iowa?",
-    options: ["Waterloo", "Fort Dodge", "Iowa City","Cedar Rapids","Des Moines"],
+    choices: ["Waterloo", "Fort Dodge", "Iowa City","Cedar Rapids","Des Moines"],
     answer: "Des Moines"},
   
   {
     question: "What is the Capital of Kansas?",
-    options: ["Colby", "Wichita", "Junction City", "Manhattan","Topeka"],
+    choices: ["Colby", "Wichita", "Junction City", "Manhattan","Topeka"],
     answer: "Topeka"},
 
   {
     question: "What is the Capital of Kentucky?",
-    options: ["Lexington","Frankfort", "Bowling Green", "Florence", "Elizabethtown"],
+    choices: ["Lexington","Frankfort", "Bowling Green", "Florence", "Elizabethtown"],
     answer: "Frankfort"},
 
   {
     question: "What is the Capital of Louisiana?",
-    options: ["Alexandria", "Lafayette","Baton Rouge", "New Orleans", "Monroe"],
+    choices: ["Alexandria", "Lafayette","Baton Rouge", "New Orleans", "Monroe"],
     answer: "Baton Rouge"},
 
   {
     question: "What is the Capital of Maine?",
-    options: ["Bangor", "Portland","Augusta", "Skowhegan", "Baileyville"],
+    choices: ["Bangor", "Portland","Augusta", "Skowhegan", "Baileyville"],
     answer: "Augusta"},
 
   {
     question: "What is the Capital of Maryland?",
-    options: ["Annapolis", "Baltimore", "Elkton", "Ocean City","Salisbury"],
+    choices: ["Annapolis", "Baltimore", "Elkton", "Ocean City","Salisbury"],
     answer: "Annapolis"},
 
   {
     question: "What is the Capital of Massachusetts?",
-    options: ["Boston", "Worcester", "Springfield", "Plymouth", "Barnstable"],
+    choices: ["Boston", "Worcester", "Springfield", "Plymouth", "Barnstable"],
     answer: "Boston"},
 
   {
     question: "What is the Capital of Michigan?",
-    options: ["Grand Rapids","Lansing", "Detroit", "Ann Arbor", "Muskegon"],
+    choices: ["Grand Rapids","Lansing", "Detroit", "Ann Arbor", "Muskegon"],
     answer: "Lansing"},
 
   {
     question: "What is the Capital of Minnesota?",
-    options: ["St. Cloud", "Duluth", "Rochester","St. Paul", "St. Olaf"],
+    choices: ["St. Cloud", "Duluth", "Rochester","St. Paul", "St. Olaf"],
     answer: "St. Paul"},
 
   {
     question: "What is the Capital of Mississippi?",
-    options: ["Jackson", "Hattiesburg", "Columbia", "Meridian", "Oxford"],
+    choices: ["Jackson", "Hattiesburg", "Columbia", "Meridian", "Oxford"],
     answer: "Jackson"},
 
   {
     question: "What is the Capital of Missouri?",
-    options: ["Chesterfield","Jefferson City", "Elsie", "Kansas City", "Columbia"],
+    choices: ["Chesterfield","Jefferson City", "Elsie", "Kansas City", "Columbia"],
     answer: "Jefferson City"},
 
   {
     question: "What is the Capital of Montana?",
-    options: ["Great Falls", "Missoula", "Butte", "Bozeman","Helena"],
+    choices: ["Great Falls", "Missoula", "Butte", "Bozeman","Helena"],
     answer: "Helena"},
 
   {
     question: "What is the Capital of Nebraska?",
-    options: ["Lincoln", "Broken Bow", "Kearney", "Omaha", "Columbus"],
+    choices: ["Lincoln", "Broken Bow", "Kearney", "Omaha", "Columbus"],
     answer: "Lincoln"},
 
   {
     question: "What is the Capital of Nevada?",
-    options: ["Carson City", "Reno", "Las Vegas", "Elko", "Winnemucca" ],
+    choices: ["Carson City", "Reno", "Las Vegas", "Elko", "Winnemucca" ],
     answer: "Carson City"},
 
   {
     question: "What is the Capital of New Hampshire?",
-    options: ["Manchester", "Nashua", "Franconia", "Woodstock","Concord"],
+    choices: ["Manchester", "Nashua", "Franconia", "Woodstock","Concord"],
     answer: "Concord"},
 
   {
     question: "What is the Capital of New Jersey?",
-    options: ["Princeton", "Trenton", "Edison", "Newark", "New Brunswick"],
+    choices: ["Princeton", "Trenton", "Edison", "Newark", "New Brunswick"],
     answer: "Trenton"},
 
   {
     question: "What is the Capital of New Mexico?",
-    options: ["Santa Fe", "Albuquerque", "Las Cruces", "Roswell", "Carlsbad"],
+    choices: ["Santa Fe", "Albuquerque", "Las Cruces", "Roswell", "Carlsbad"],
     answer: "Santa Fe"},
 
   {
     question: "What is the Capital of New York?",
-    options: ["Albany", "New York City", "Buffalo", "Syracuse", "Rochester"],
+    choices: ["Albany", "New York City", "Buffalo", "Syracuse", "Rochester"],
     answer: "Albany"},
 
   {
     question: "What is the Capital of North Carolina?",
-    options: ["Charlotte","Raleigh", "Greenville", "Wilmington", "Winston-Salem"],
+    choices: ["Charlotte","Raleigh", "Greenville", "Wilmington", "Winston-Salem"],
     answer: "Raleigh"},
 
   {
     question: "What is the Capital of North Dakota?",
-    options: ["Steele", "Dickinson", "Bismarck", "Valley City", "Jamestown"],
+    choices: ["Steele", "Dickinson", "Bismarck", "Valley City", "Jamestown"],
     answer: "Bismarck"},
 
   {
     question: "What is the Capital of Ohio?",
-    options: ["Columbus", "Springfield", "Dayton", "Akron", "Cleveland"],
+    choices: ["Columbus", "Springfield", "Dayton", "Akron", "Cleveland"],
     answer: "Columbus"},
 
   {
     question: "What is the Capital of Oklahoma?",
-    options:["Broken Arrow", "Muskogee","Oklahoma City", "Lawton", "Bartlesville"],
+    choices:["Broken Arrow", "Muskogee","Oklahoma City", "Lawton", "Bartlesville"],
     answer: "Oklahoma City"},
 
   {
     question: "What is the Capital of Oregon?",
-    options: ["Salem", "Portland", "Newport", "Bend", "La Grande"],
+    choices: ["Salem", "Portland", "Newport", "Bend", "La Grande"],
     answer: "Salem"},
 
   {
     question: "What is the Capital of Pennsylvannia?",
-    options: ["Philadelphia", "Pittsburgh", "Harrisburg", "Punxsutawney", "Reading"],
+    choices: ["Philadelphia", "Pittsburgh", "Harrisburg", "Punxsutawney", "Reading"],
     answer: "Harrisburg"},
 
   {
     question: "What is the Capital of Rhode Island?",
-    options: ["Providence", "Narragansett", "Pawtucket", "Warwick", "Woonsocket"],
+    choices: ["Providence", "Narragansett", "Pawtucket", "Warwick", "Woonsocket"],
     answer: "Providence"},
 
   {
     question: "What is the Capital of South Carolina?",
-    options: ["Charleston", "Spartanburg", "Camden","Columbia", "Florence"],
+    choices: ["Charleston", "Spartanburg", "Camden","Columbia", "Florence"],
     answer: "Columbia"},
 
   {
     question: "What is the Capital of South Dakota?",
-    options: ["Rapid City", "Aberdeen", "Sioux Falls", "Chamberlain","Pierre"],
+    choices: ["Rapid City", "Aberdeen", "Sioux Falls", "Chamberlain","Pierre"],
     answer: "Pierre"},
 
   {
     question: "What is the Capital of Tennessee?",
-    options: ["Nashville", "Chattanooga", "Knoxville", "Jackson", "Memphis"],
+    choices: ["Nashville", "Chattanooga", "Knoxville", "Jackson", "Memphis"],
     answer: "Nashville"},
 
   {
     question: "What is the Capital of Texas?",
-    options: ["Lubbock", "Odessa","Austin", "San Antonio", "Houston"],
+    choices: ["Lubbock", "Odessa","Austin", "San Antonio", "Houston"],
     answer: "Austin"},
 
   {
     question: "What is the Capital of Utah?",
-    options: ["Salt Lake City", "Ogden", "Moab", "Provo", "Logan"],
+    choices: ["Salt Lake City", "Ogden", "Moab", "Provo", "Logan"],
     answer: "Salt Lake City"},
 
   {
     question: "What is the Capital of Vermont?",
-    options: ["Middlebury", "Greensboro", "Burlington","Montpelier", "Jay"],
+    choices: ["Middlebury", "Greensboro", "Burlington","Montpelier", "Jay"],
     answer: "Montpelier"},
 
   {
     question:"What is the Capital of Virginia?",
-    options: ["Richmond", "Newport News", "Lynchburg", "Charlottesville", "Roanoke"],
+    choices: ["Richmond", "Newport News", "Lynchburg", "Charlottesville", "Roanoke"],
     answer: "Richmond"},
 
   {
     question: "What is the Capital of Washington?",
-    options: ["Tacoma", "Seattle", "Yakima", "Spokane","Olympia"],
+    choices: ["Tacoma", "Seattle", "Yakima", "Spokane","Olympia"],
     answer: "Olympia"},
 
   {
     question: "What is the Capital of West Virginia?",
-    options: ["Morgantown","Charleston", "Fayetteville", "Bridgeport", "Lewisburg"],
+    choices: ["Morgantown","Charleston", "Fayetteville", "Bridgeport", "Lewisburg"],
     answer: "Charleston"},
 
   {
     question: "What is the Capital of Wisconsin?",
-    options: ["Oshkosh","Madison", "Eau Claire", "Milwaukee", "Kenosha"],
+    choices: ["Oshkosh","Madison", "Eau Claire", "Milwaukee", "Kenosha"],
     answer: "Madison"},
 
   {
     question: "What is the Capital of Wyoming?",
-    options: ["Cheyenne", "Casper", "Rock Springs", "Douglas", "Buffalo"],
+    choices: ["Cheyenne", "Casper", "Rock Springs", "Douglas", "Buffalo"],
     answer: "Cheyenne"},
 
   
